cacb8351dcdd43ce16de16e2538dd7b768db85a7
Tyson Bailey <onaclov2000+github@gmail.com>
2014-12-22 08:44:55 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,21 @@
 var app = angular.module("myapp", ["test"])
        .controller('MyController', ['$scope', '$test', ,
            function($scope, $test) {
-           $scope.go = function ('http://www.antennaweb.org/Stations.aspx?Address=&City=Albuquerque&State=NM&ZIP=87114&Housing=S&Accuracy=4&Height=6&Obstructed=False&StationList=&Lat=35.1958704&Lon=-106.6939803'){
+           $scope.go = function () {
+                  var response = $test('http://www.antennaweb.org/Stations.aspx?Address=&City=Albuquerque&State=NM&ZIP=87114&Housing=S&Accuracy=4&Height=6&Obstructed=False&StationList=&Lat=35.1958704&Lon=-106.6939803');
+                   response
+                       .success(function(data, status) {
+                           console.log('SUCCESS' + data);
+                           $scope.data = data;
+
+                           $scope.status = status;
+                       })
+                       .error(function(data, status) {
+                           console.log('ERROR' + status);
+                           $scope.data = data;
+                           $scope.status = status;
+                       });
+              }     
            }
            ]);
    app.config(function($httpProvider) {
